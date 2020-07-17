1e7c198756f39031f9508f76e56307ae67a9f75d
fathimafameena <fathima.fameena@yahoo.com>
2020-07-17 08:30:56 -0500

    Stated with the constructor code

---
+++ OnlineVideos/src/onlineTutorial/Constructor.java
@@ -0,0 +1,20 @@
+package onlineTutorial;
+
+public class Constructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//constructor is used to allocate the memory to the object 
+		//it has same name as of class name.
+		//it is a method that never return anything.
+		//intialize the object as soon as you create the object
+		// we can also have two constructor with different parameters
+		//this concept of having multiple constructor with different parameters
+		//is called is called constructor overloading
+		
+		
+
+	}
+
+}
--- OnlineVideos/src/onlineTutorial/ObjectDemo.java
+++ OnlineVideos/src/onlineTutorial/ObjectDemo.java
@@ -5,8 +5,27 @@ public class ObjectDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Calc obj=new Calc();//ref for the calc class
+		obj.num1=3;
+		obj.num2=4;
+		int output=obj.add();
+		
+		System.out.println("the output of add is"+output);
 		
 
 	}
 
+}
+  class Calc{
+	int num1;
+	int num2;
+	int result;
+	
+	public int add() {
+		
+		result=num1+num2;
+		return result;
+	}
+	
+	
 }
